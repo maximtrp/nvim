f12320186ed53512c8537705ceab42769b0b75b1
Maksim Terpilovskii <maximtrp@gmail.com>
2025-04-01 20:52:25 +0200

    reverted to find_files on launch

--- lua/config/other.lua
+++ lua/config/other.lua
@@ -49,7 +49,7 @@ vim.api.nvim_create_autocmd("VimEnter", {
 			})
 		elseif vim.fn.argc() == 0 then
 			vim.cmd("bd")
-			require("telescope.builtin").git_files({ hidden = true })
+			require("telescope.builtin").find_files({ hidden = true })
 		end
 	end,
 })
