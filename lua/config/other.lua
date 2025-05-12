dbe23fc4d3ff468ceb4bea065a3c965ab818f5cb
Maksim Terpilovskii <maximtrp@gmail.com>
2025-05-12 11:11:38 +0200

    updated lsp and fold settings

--- lua/config/other.lua
+++ lua/config/other.lua
@@ -16,6 +16,25 @@ vim.lsp.config("ts_ls", {
 			},
 		},
 	},
+	filetypes = {
+		"javascript",
+		"typescript",
+		"vue",
+	},
+})
+
+vim.lsp.config("volar", {
+	init_options = {
+		typescript = {
+			tsdk = "/opt/homebrew/lib/node_modules/typescript/lib",
+		},
+	},
+	before_init = function(_, config)
+		local lib_path = vim.fs.find("node_modules/typescript/lib", { path = config.root_dir, upward = true })[1]
+		if lib_path then
+			config.init_options.typescript.tsdk = lib_path
+		end
+	end,
 })
 
 vim.lsp.config("pylsp", {
--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -18,11 +18,12 @@ vim.o.pumheight = 15
 vim.o.undofile = true
 vim.o.foldenable = true
 vim.o.foldlevel = 99
-vim.o.foldlevelstart = 99
+vim.o.foldlevelstart = 9
+vim.o.foldnestmax = 20
+vim.o.foldcolumn = "1"
+-- vim.o.foldmethod = "indent"
 vim.o.foldmethod = "expr"
 vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.o.foldtext = ""
-vim.o.foldcolumn = "1"
 vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 -- vim.o.showtabline = 2
-
